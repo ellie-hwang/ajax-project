6da5844a189c5e10288379c40166c89a29cfe577
Eleonora Hwang <eleonorahwang@gmail.com>
2022-03-01 20:34:48 -0800

    Use a loop to create a DOM tree for each search result in the HTTP response and append it to the page when the submit button is clicked

--- js/main.js
+++ js/main.js
@@ -13,40 +13,46 @@ function handleSearch(event) {
   xhr.open('GET', 'http://www.omdbapi.com/?s=' + $movieTitle + '&apikey=fd3f5e28');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    // var $searchResultsRaw = xhr.response;
-
+    var $searchResultsRaw = xhr.response.Search;
+    for (let i = 0; i < $searchResultsRaw.length; i++) {
+      var $result = renderSearchResult($searchResultsRaw[i]);
+      $resultsList.appendChild($result);
+    }
   });
 
   xhr.send();
   data.view = 'results-view';
 }
 
-// function renderSearchResult(resultObj) {
-//   var $liEl = document.createElement('li');
-//   $liEl.className = 'result-card pad-1-bot marg-1-bot column-fourth';
-//   var $divEl1 = document.createElement('div');
-//   $divEl1.className = 'search-img-container';
-//   var $imgEl = document.createElement('img');
-//   $imgEl.setAttribute('src', resultObj.Search.Poster);
-//   var $iEl = document.createElement('i');
-//   $iEl.className = 'fas fa-info-circle';
-//   var $divEl2 = document.createElement('div');
-//   $divEl2.className = 'pad-1-2-left-right text-align-center';
-//   var $h3El = document.createElement('h3');
-//   $h3El.className = 'result-title';
-//   $h3El.textContent = resultObj.Search.Title;
-//   var $buttonEl = document.createElement('button');
-//   $buttonEl.className = 'button-style review-button';
-//   $buttonEl.textContent = 'REVIEW';
+var $resultsList = document.querySelector('#results-list');
 
-//   $divEl1.appendChild($imgEl);
-//   $divEl1.appendChild($iEl);
+function renderSearchResult(resultObj) {
+  var $liEl = document.createElement('li');
+  $liEl.className = 'result-card pad-1-bot marg-1-bot column-fourth';
+  var $divEl1 = document.createElement('div');
+  $divEl1.className = 'search-img-container';
+  var $imgEl = document.createElement('img');
+  $imgEl.className = 'search-img';
+  $imgEl.setAttribute('src', resultObj.Poster);
+  var $iEl = document.createElement('i');
+  $iEl.className = 'fas fa-info-circle';
+  var $divEl2 = document.createElement('div');
+  $divEl2.className = 'pad-1-2-left-right text-align-center';
+  var $h3El = document.createElement('h3');
+  $h3El.className = 'result-title';
+  $h3El.textContent = resultObj.Title;
+  var $buttonEl = document.createElement('button');
+  $buttonEl.className = 'button-style review-button';
+  $buttonEl.textContent = 'REVIEW';
 
-//   $divEl2.appendChild($h3El);
-//   $divEl2.appendChild($buttonEl);
+  $divEl1.appendChild($imgEl);
+  $divEl1.appendChild($iEl);
 
-//   $liEl.appendChild($divEl1);
-//   $liEl.appendChild($divEl2);
+  $divEl2.appendChild($h3El);
+  $divEl2.appendChild($buttonEl);
 
-//   return $liEl;
-// }
+  $liEl.appendChild($divEl1);
+  $liEl.appendChild($divEl2);
+
+  return $liEl;
+}
