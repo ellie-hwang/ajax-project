a0b021277cb90fe19c7d47f249ea386cb17e1f57
Eleonora Hwang <eleonorahwang@gmail.com>
2022-03-04 12:55:21 -0800

    Ensure that submitting a new review also creates a new DOM tree for it and adds it to the page. Ensure that submitting a new review automatically shows the 'reviews' view without reloading the page.

--- js/main.js
+++ js/main.js
@@ -321,6 +321,7 @@ function createReview(event) {
   for (let i = 0; i < $stars.length; i++) {
     $stars[i].className = 'far fa-star star-icon';
   }
+  $reviewList.prepend(renderReview(reviewObj));
   $starRating.setAttribute('data-star', '0');
   $reviewForm.reset();
   resetSearchBar();
@@ -352,22 +353,6 @@ function renderReview(reviewObj) {
     $divEl2.appendChild($iEl2);
   }
 
-  // var $iEl1 = document.createElement('i');
-  // $iEl1.className = 'far fa-star star-icon-review';
-  // $iEl1.setAttribute('data-star', '1');
-  // var $iEl2 = document.createElement('i');
-  // $iEl2.className = 'far fa-star star-icon-review';
-  // $iEl2.setAttribute('data-star', '2');
-  // var $iEl3 = document.createElement('i');
-  // $iEl3.className = 'far fa-star star-icon-review';
-  // $iEl3.setAttribute('data-star', '3');
-  // var $iEl4 = document.createElement('i');
-  // $iEl4.className = 'far fa-star star-icon-review';
-  // $iEl4.setAttribute('data-star', '4');
-  // var $iEl5 = document.createElement('i');
-  // $iEl5.className = 'far fa-star star-icon-review';
-  // $iEl5.setAttribute('data-star', '5');
-
   var $divEl3 = document.createElement('div');
   $divEl3.className = 'pad-1';
   var $h3El = document.createElement('h3');
@@ -381,11 +366,6 @@ function renderReview(reviewObj) {
   $iEl6.className = 'fas fa-pen-alt pen-icon';
 
   $divEl1.appendChild($imgEl);
-  // $divEl2.appendChild($iEl1);
-  // $divEl2.appendChild($iEl2);
-  // $divEl2.appendChild($iEl3);
-  // $divEl2.appendChild($iEl4);
-  // $divEl2.appendChild($iEl5);
   $divEl3.appendChild($h3El);
   $divEl3.appendChild($pEl);
   $divEl4.appendChild($iEl6);
